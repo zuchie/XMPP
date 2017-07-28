b9ff39469c69047debb1ebd2b020f45db488752a
zuchie <zuchie@gmail.com>
2017-07-28 00:57:01 -0700

    first commit

--- ButterflyMX/LoginViewController.swift
+++ ButterflyMX/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import XMPPFramework
 
 class ViewController: UIViewController {
 
---
+++ ButterflyMX/XMPPStream.swift
@@ -0,0 +1,9 @@
+//
+//  XMPPStream.swift
+//  ButterflyMX
+//
+//  Created by Zhe Cui on 7/27/17.
+//  Copyright © 2017 Zhe Cui. All rights reserved.
+//
+
+import Foundation
